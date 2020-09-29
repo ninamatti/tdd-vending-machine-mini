cbc16f0f81fa88159e3cb890648efb5e45d28261
Nina Matti <ninadesireematti@gmail.com>
2020-09-30 05:24:22 +0900

    added code manually for testing methods and property existence as well as code that passes tests that was co-authored by ninamatti and Jaymontojo673

--- VendingMachine.js
+++ VendingMachine.js
@@ -1,4 +1,29 @@
-// your class here
+class VendingMachine {
+  constructor() {
+    this.balance = 0;
+    this.till = { 10: 0, 50: 0, 100: 0, 500: 0 };
+    this.selectRow = 0;
+    this.selectColumn = 0;
+  }
+  insertCoins(denomination) {
+    this.balance += denomination;
+    this.till.denomination += 1;
+    return true;
+  }
+  changeReturn() {
+    //logs coins
+    //resets balance
+    return true;
+  }
+  selector() {
+    // selects item from given row/column
+    // prints item 
+    return true;
+  }
+}
+
+module.exports = VendingMachine;
+
 /*
   >>> Don't forget to use module.exports!
   What is that? Well, glad you asked.
--- test/test.js
+++ test/test.js
@@ -1,14 +1,53 @@
 const VendingMachine = require("../VendingMachine");
 const { expect } = require("chai");
 
+describe("Insert Coins", () => {
+  const machine = new VendingMachine();
+  it("should be a method of vending machine", () => {
+    expect(typeof machine.insertCoins).to.equal("function"); // Use an ES6 getter
+  });
+  it("should increase balance by the coin value", () => {
+    machine.insertCoins(10);
+    expect(machine.balance).to.equal(10);
+  });
+  it("should store coin in till based on denomination", () => {
+    machine.insertCoins(10);
+    console.log(machine.till["10"]);
+    expect(machine.till["10"]).to.equal(1); // Use an ES6 getter
+  });
+});
+
+describe("Change Return", () => {
+  it("should be a method of vending machine", () => {
+    const machine = new VendingMachine();
+    expect(typeof machine.changeReturn).to.equal("function"); // Use an ES6 getter
+  });
+});
+
+describe("Selector", () => {
+  it("should be a method of vending machine", () => {
+    const machine = new VendingMachine();
+    expect(typeof machine.selector).to.equal("function"); // Use an ES6 getter
+  });
+});
+
+describe("Vending machine", () => {
+  it("should have the necessary properties", () => {
+    const machine = new VendingMachine();
+    expect(machine).to.have.property("balance"); // Use an ES6 getter
+    expect(machine).to.have.property("till"); // Use an ES6 getter
+    expect(machine).to.have.property("selectRow"); // Use an ES6 getter
+    expect(machine).to.have.property("selectColumn"); // Use an ES6 getter
+  });
+});
+
 describe("vending machine", () => {
   it("should accept valid coins", () => {
     // Setup
     const machine = new VendingMachine();
-
+    console.log(machine);
     // Exercise
     machine.insertCoin(500);
-
     // Assert
     expect(machine.till).to.deep.equal({
       10: 0,
