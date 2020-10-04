3d6548ed2ef0625991ff9f237d711a91b3dd020c
Nina Matti <ninadesireematti@gmail.com>
2020-10-04 16:09:34 +0900

    added inventory and product details

--- VendingMachine.js
+++ VendingMachine.js
@@ -7,7 +7,7 @@ class VendingMachine {
   }
   insertCoins(denomination) {
     this.balance += denomination;
-    this.till.denomination += 1;
+    this.till[denomination] += 1;
     return true;
   }
   changeReturn() {
@@ -15,12 +15,45 @@ class VendingMachine {
     //resets balance
     return true;
   }
-  selector() {
+
+  selector(row, column) {
     // selects item from given row/column
-    // prints item 
+    if ((this.selectRow = 0)) {
+      this.selectRow = row;
+      this.selectColumn = column;
+    }
+
+    // prints item
     return true;
   }
 }
+
+const orangeJuice = { name: "orangeJuice", price: 150, count: 5 };
+const chocoMilk = { name: "chocoMilk", price: 250, count: 5 };
+const toblerone = { name: "toblerone", price: 350, count: 5 };
+const gummibears = { name: "gummibears", price: 200, count: 5 };
+
+const calpis = { name: "calpis", price: 250, count: 5 };
+const kagiFret = { name: "kagiFret", price: 150, count: 5 };
+const potatoChips = { name: "potatoChips", price: 100, count: 5 };
+const chewingGum = { name: "chewingGum", price: 100, count: 5 };
+
+const iceTeaPeach = { name: "iceTeaPeach", price: 300, count: 5 };
+const mAndM = { name: "mAndM", price: 250, count: 5 };
+const snickers = { name: "snickers", price: 100, count: 5 };
+const applePie = { name: "applePie", price: 250, count: 5 };
+
+const crackers = { name: "crackers", price: 200, count: 5 };
+const bounty = { name: "bounty", price: 050, count: 5 };
+const fanta = { name: "fanta", price: 250, count: 5 };
+const ricola = { name: "ricola", price: 350, count: 5 };
+
+const inventory = [
+  [orangeJuice, chocoMilk, toblerone, gummibears],
+  [calpis, kagiFret, potatoChips, chewingGum],
+  [iceTeaPeach, mAndM, snickers, applePie],
+  [crackers, bounty, fanta, ricola],
+];
 
 module.exports = VendingMachine;
 
